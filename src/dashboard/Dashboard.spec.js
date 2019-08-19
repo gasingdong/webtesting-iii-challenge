44ce60932e4f2f411713355abae0a1a144c74456
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-19 16:06:04 -0700

    add dashboard testing

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
-// Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ReactDOM from 'react-dom';
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Dashboard />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it("renders without crashing", () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Dashboard />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 import Display from './Display';
 
