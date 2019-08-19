25739f564c2739fa1182522cd2e4873f12513b19
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-19 15:53:17 -0700

    add display testing

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -9,4 +9,16 @@ describe('<Display />', () => {
     const tree = renderer.create(<Display />);
     expect(tree.toJSON()).toMatchSnapshot();
   });
+
+  it("displays closed and locked props", () => {
+    const { getByText } = render(<Display closed={true} locked={true} />);
+    expect(getByText(/closed/i));
+    expect(getByText(/locked/i));
+  })
+
+  it("displays open and unlocked props", () => {
+    const { getByText } = render(<Display closed={false} locked={false} />);
+    expect(getByText(/open/i));
+    expect(getByText(/unlocked/i));
+  })
 })
